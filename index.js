94533f77f2646809f7d9e4d4e86684d8e28f0a80
Christopher Bero <bigbero@gmail.com>
2016-07-22 15:58:05 -0500

    basic idea

---
+++ index.js
@@ -0,0 +1,15 @@
+function download(filename, text) {
+    console.log("Doing download");
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+window.onload(download("test2.cfg", "hello"));
