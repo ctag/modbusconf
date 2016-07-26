0ee29b94dfba988a862ccf1b9aa45799630b2110
Christopher Bero <bigbero@gmail.com>
2016-07-26 15:22:46 -0500

    Editing values works, no error checking.

--- index.js
+++ index.js
@@ -41,6 +41,24 @@ function download(filename, text) {
     document.body.removeChild(element);
 }
 
+function get_title(dev)
+{
+    var text = dev.slave_id + " - " + dev.name;
+    return text;
+}
+
+function get_dev(text)
+{
+    for (var dev in devices)
+    {
+        var compare = get_title(devices[dev]);
+        if (compare === text)
+        {
+            return dev;
+        }
+    }
+}
+
 function update_list()
 {
     var list = $("#config_list");
@@ -51,9 +69,44 @@ function update_list()
         var elem = "<div id=\"";
         elem += "device_" + devices[dev].slave_id + "\"";
         elem += " class=\"config_list_item\">";
-        elem += devices[dev].slave_id + " - " + devices[dev].name + "</div>";
+        elem += get_title(devices[dev]) + "</div>";
         list.append(elem);
     }
+    $(".config_list_item").click(function(e) {
+        $(".config_list_item").removeClass("config_list_item_selected");
+        var text = e.target.textContent;
+        for (var dev in devices)
+        {
+            var compare = get_title(devices[dev]);
+            if (compare === text)
+            {
+                set_values(devices[dev]);
+                $(this).addClass("config_list_item_selected");
+                break;
+            }
+        }
+    });
+}
+
+function set_values(dev)
+{
+    $('#device-name').val(dev.name);
+    $('#device-protocol').val(dev.protocol);
+    $('#device-id').val(dev.slave_id);
+    $('#device-address').val(dev.address);
+    $('#device-port').val(dev.IP_Port);
+    $('#device-rtu-baud').val(dev.RTU_Baud_Rate);
+    $('#device-rtu-parity').val(dev.RTU_Parity);
+    $('#device-rtu-data').val(dev.RTU_Data_Bits);
+    $('#device-rtu-stop').val(dev.RTU_Stop_Bits);
+    $('#device-input-registers-start').val(dev.Input_Registers_Start);
+    $('#device-input-registers-size').val(dev.Input_Registers_Size);
+    $('#device-coils-start').val(dev.Coils_Start);
+    $('#device-coils-size').val(dev.Coils_Size);
+    $('#device-input-registers-start').val(dev.Input_Registers_Start);
+    $('#device-input-registers-size').val(dev.Input_Registers_Size);
+    $('#device-holding-registers-start').val(dev.Holding_Registers_Start);
+    $('#device-holding-registers-size').val(dev.Holding_Registers_Size);
 }
 
 $("#config_list_new").click(function(e) {
@@ -88,4 +141,38 @@ $("#button_download").click(function(e) {
     text += 'device0.Holding_Registers_Size = "' + $('#device-holding-registers-size').val() + '"\n';
 
     download("modbusdevice.cfg", text);
+});
+
+$('.config_values_input').change(function(e) {
+    var elem = $('.config_list_item_selected');
+    console.log(elem.text());
+    var dev = get_dev(elem.text());
+    var text;
+    if ($('#device-name').val() !== devices[dev].name)
+    {
+        devices[dev].name = $('#device-name').val();
+        text = get_title(devices[dev]);
+        elem.text(text);
+    }
+    if ($('#device-id').val() !== devices[dev].slave_id)
+    {
+        devices[dev].slave_id = $('#device-id').val();
+        text = get_title(devices[dev]);
+        elem.text(text);
+    }
+    devices[dev].IP_Port = $('#device-port').val();
+    devices[dev].protocol = $('#device-protocol').val();
+    devices[dev].address = $('#device-address').val();
+    devices[dev].RTU_Baud_Rate = $('#device-rtu-baud').val();
+    devices[dev].RTU_Parity = $('#device-rtu-parity').val();
+    devices[dev].RTU_Data_Bits = $('#device-rtu-data').val();
+    devices[dev].RTU_Stop_Bits = $('#device-rtu-stop').val();
+    devices[dev].Input_Registers_Start = $('#device-input-registers-start').val();
+    devices[dev].Input_Registers_Size = $('#device-input-registers-size').val();
+    devices[dev].Coils_Start = $('#device-coils-start').val();
+    devices[dev].Coils_Size = $('#device-coils-size').val();
+    devices[dev].Input_Registers_Start = $('#device-input-registers-start').val();
+    devices[dev].Input_Registers_Size = $('#device-input-registers-size').val();
+    devices[dev].Holding_Registers_Start = $('#device-holding-registers-start').val();
+    devices[dev].Holding_Registers_Size = $('#device-holding-registers-size').val();
 });
